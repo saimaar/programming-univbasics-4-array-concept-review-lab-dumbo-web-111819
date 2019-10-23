ec1c7c8aea14acb311e8943601d533b2a65bb671
Saima Rahman <rahman.saima92@gmail.com>
2019-10-23 15:26:03 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,13 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+ return array.index(value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+max_num = array.max
+return max_num
 end
 
 def find_min_value(array)
-  # Add your solution here
+  min_num = array.min
+  return min_num
 end
